4691c3b1857c28dfd673aa03970d311c698bbf7e
GeraHeeHee <gerardomdc2@gmail.com>
2026-05-18 22:16:41 -0700

    cambio 3

--- player.gd
+++ player.gd
@@ -1,13 +1,24 @@
 extends RigidBody3D
-@export var camera_y_offset: float = 0.5
+@onready var spring_arm_3d: SpringArm3D = $SpringArm3D
+var speed = 0.01
+var mouse = 0.01
+@export var camera_y_offset: float = 1
+
+func _input(event: InputEvent):
+	if event is InputEventMouseMotion:
+		spring_arm_3d.rotation.x -= event.relative.y * mouse
+		spring_arm_3d.rotation.y -= event.relative.x * mouse
+		#spring_arm_3d.rotation.x = clamp(spring_arm_3d.rotation.x, -PI/2, PI/4)
+		spring_arm_3d.rotation.x = clamp(spring_arm_3d.rotation.x, deg_to_rad(-60), PI/4)
 
 func _ready() -> void:
-	pass
+	spring_arm_3d.top_level = true
 
 func _process(delta: float) -> void:
 	var target_position = self.global_position
 	target_position.y += camera_y_offset
 	
+	spring_arm_3d.global_position = target_position
 	
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	var inputDirection = Vector3(
@@ -15,4 +26,6 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 		0,
 		Input.get_axis("up","down")
 	)
+	inputDirection = inputDirection.rotated(Vector3.UP, spring_arm_3d.rotation.y).normalized() * speed
+	
 	apply_central_impulse(Vector3(inputDirection.x, 0, inputDirection.z))
