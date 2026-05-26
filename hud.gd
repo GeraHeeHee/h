9a3bd7b708f90849068d205914781f2afbc12077
GeraHeeHee <gerardomdc2@gmail.com>
2026-05-25 17:43:40 -0700

    cambio 6: Elaboración del HUD y recolección de puntos (puntaje)

--- hud.gd
+++ hud.gd
@@ -1,8 +1,8 @@
 extends Node
-@onready var score: Label = $Control/MarginContainer/score
+@onready var label: Label = $Control/MarginContainer/label
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	score.text = "Score: " + str(GameManager.getPoint())
+	label.text = "Score: " + str(GameManager.getPoint())
